842f07fff470f064d9143f8d87dd2fbc82ad2e9e
k395599 <keigo1003560@gmail.com>
2025-01-22 23:41:58 +0000

    mornig study

--- ABC/300~399/384_B.py
+++ ABC/300~399/384_B.py
@@ -1,2 +1,13 @@
-# %%
-N=input()
+n, r=map(int, input().split())
+a =[list(map(int,input().split())) for _ in range(n)]
+
+
+for i in range(n):
+    if a[i][0]==1:
+        if 1600<=r<=2799:
+            r+=a[i][1]
+    else:
+        if 1200<=r<=2399:
+            r+=a[i][1]
+
+print(r)
--- ABC/300~399/385_B.py
+++ ABC/300~399/385_B.py
@@ -1,6 +1,44 @@
 # %%
-H, W, X, Y =map(int, input().split())
-S =[input() for _ in range(H)]
-T = str(input())
+h, w, x, y =map(int, input().split())
+s =[input() for _ in range(h)]
+t = str(input())
+x=x-1
+y=y-1
+home=[]
+for i in t:
+    if i=='U':
+        if s[x-1][y]=='.':
+            x-=1
+        elif s[x-1][y]=='@':
+            x-=1
+            home.append((x,y))
+        else:
+            continue
+    elif i=='D':
+        if s[x+1][y]=='.':
+            x+=1
+        elif s[x+1][y]=='@':
+            x+=1
+            home.append((x,y))
+        else:
+            continue
+    elif i=='L':
+        if s[x][y-1]=='.':
+            y-=1
+        elif s[x][y-1]=='@':
+            y-=1
+            home.append((x,y))
+        else:
+            continue
+    elif i=='R':
+        if s[x][y+1]=='.':
+            y+=1
+        elif s[x][y+1]=='@':
+            y+=1
+            home.append((x,y))
+        else:
+            continue
 
-print(S)
+print(x+1,y+1,len(set(home)))
+
+# %%
