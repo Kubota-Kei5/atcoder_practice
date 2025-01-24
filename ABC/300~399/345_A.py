4ea6e7f548ced17fa436c9e207b535a936513f87
k395599 <keigo1003560@gmail.com>
2025-01-24 00:01:35 +0000

    mornig study

---
+++ ABC/300~399/345_A.py
@@ -0,0 +1,14 @@
+# %%
+S=str(input())
+
+if S[0]!='<' or S[len(S)-1]!='>':
+    print('No')
+else:
+    for i in range(1,len(S)-1):
+        if S[i]=='=':
+            pass
+        else:
+            print('No')
+            break
+    print('Yes')
+# %%
---
+++ ABC/300~399/383_B.py
@@ -0,0 +1,26 @@
+# %%
+from itertools import combinations
+
+h,w,d=map(int,input().split())
+s=[list(input()) for _ in range(h)]
+floors=[]
+
+for i in range(h):
+    for j in range(w):
+        if s[i][j]=='.':
+            floors.append((i,j))
+        else:
+            continue
+
+max_humidified=0
+
+for (x1,y1),(x2,y2) in combinations(floors,2):
+    humidified=set()
+    for x,y in floors:
+        if abs(x-x1)+abs(y-y1)<=d or abs(x-x2)+abs(y-y2)<=d:
+            humidified.add((x,y))
+    max_humidified=max(max_humidified, len(humidified))
+
+print(max_humidified)
+
+# %%
