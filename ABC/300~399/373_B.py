bd774cf51f1da8960b7b105cf1762efa2cfc56b4
k395599 <keigo1003560@gmail.com>
2025-01-25 11:30:21 +0000

    一発AC

--- ABC/300~399/373_B.py
+++ ABC/300~399/373_B.py
@@ -1,2 +1,19 @@
 # %%
-N=input()
+S=str(input())
+S_li=[s for s in S]
+
+A='ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+A_li=[a for a in A]
+
+pos=[]
+for a in A:
+    for s in S:
+        if a==s:
+            pos.append(S.index(s))
+
+cnt=0
+for i in range(len(pos)-1):
+    cnt+=abs(pos[i]-pos[i+1])
+
+print(cnt)
+# %%
