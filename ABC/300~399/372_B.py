6babbf63c265ab29def557c3e8bd968fea82a78b
k395599 <keigo1003560@gmail.com>
2025-01-25 11:53:00 +0000

    解きなおし

--- ABC/300~399/372_B.py
+++ ABC/300~399/372_B.py
@@ -1,2 +1,17 @@
 # %%
-N=input()
+M=int(input())
+A=[]
+i=10
+
+while True:
+    if M==0:
+        break
+    if 3**i>M:
+        i-=1
+        continue
+    M-=3**i
+    A.append(i)
+
+print(len(A))
+print(*A)
+# %%
