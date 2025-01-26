c0b37b4784aa51850f382ed4017df9d9adb5fa03
k395599 <keigo1003560@gmail.com>
2025-01-26 02:17:30 +0000

    Complete

--- ABC/300~399/371_B.py
+++ ABC/300~399/371_B.py
@@ -1,2 +1,21 @@
 # %%
-N=input()
+n,m = map(int, input().split())
+A=[]
+B=[]
+
+for i in range(m):
+    a,b=map(str,input().split())
+    A.append(int(a))
+    B.append(b)
+
+# 各家の男の子の人数をカウント
+l=[0]*n
+
+for i in range(m):
+    if B[i]=='M' and l[A[i]-1]==0:
+        print('Yes')
+        l[A[i]-1]+=1
+    else:
+        print('No')
+
+# %%
