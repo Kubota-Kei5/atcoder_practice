5ceef39d7b0df4aa95564a1dd775e372dc911cfa
k395599 <keigo1003560@gmail.com>
2025-01-26 13:21:17 +0000

    complete

--- ABC/300~399/370_B.py
+++ ABC/300~399/370_B.py
@@ -1,2 +1,20 @@
 # %%
-N=input()
+N=int(input())
+A=[]
+for i in range(N):
+    X=list(map(int,input().split()))
+    A.append(X)
+add=1
+now=1
+
+while add<N+1:
+    if now>=add:
+        now=A[now-1][add-1]
+    else:
+        now=A[add-1][now-1]
+    add+=1
+
+print(now)
+
+
+# %%
