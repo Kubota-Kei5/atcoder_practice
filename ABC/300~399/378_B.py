9f548530f3e0397a505ae093f8cde73d0d0d02a1
k395599 <keigo1003560@gmail.com>
2025-01-26 13:21:40 +0000

    retry

--- ABC/300~399/378_B.py
+++ ABC/300~399/378_B.py
@@ -1,12 +1,34 @@
 # %%
-n=int(input())
+N=int(input())
 x=[]
-for i in range(n):
+for i in range(N):
     a,b=map(int,input().split())
     x.append([a,b])
 
-q=int(input())
+Q=int(input())
 y=[]
-for i in range(q):
+for i in range(Q):
     a,b=map(int,input().split())
     y.append([a,b])
+
+for j in range(Q):
+    d=y[j][1]
+    q=x[y[j][0]-1][0]
+    r=x[y[j][0]-1][1]
+    
+    if d<=r:
+        print(r)
+    elif d%q==r:
+        print(d)
+    else:
+        for i in range(30):
+            if d<i*q+r:
+                print(i*q+r)
+                break
+            else:
+                pass
+
+# %%
+a=2
+print(a//5)
+# %%
