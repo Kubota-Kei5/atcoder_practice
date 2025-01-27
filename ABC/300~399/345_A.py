5019112f6c5e080e7e99e35819f9a218e87902b9
k395599 <keigo1003560@gmail.com>
2025-01-27 13:33:32 +0000

    complete

--- ABC/300~399/345_A.py
+++ ABC/300~399/345_A.py
@@ -1,13 +1,7 @@
 # %%
 S=str(input())
 
-if S[0]!='<' or S[len(S)-1]!='>':
-    print('No')
-else:
-    for i in range(1,len(S)-1):
-        if S[i]=='=':
-            pass
-        else:
-            print('No')
-            break
+if S[0]=='<' and S[-1]=='>' and S[1:-1]=='='*(len(S)-2):
     print('Yes')
+else:
+    print('No')
