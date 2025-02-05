a1c857ad7267c76d9a4d044c6b73a59bb9f4a313
k395599 <keigo1003560@gmail.com>
2025-02-05 11:54:59 +0000

    Retry

---
+++ ABC/300~399/360_B.py
@@ -0,0 +1,14 @@
+# %%
+def check_subsequence(s,t):
+    for w in range(1, len(s)):
+        for c in range(w):
+            now = []
+            for i in range(c, len(s), w):
+                now.append(s[i])
+            if "".join(now) == t:
+                return "Yes"
+    return "No"
+
+if __name__ == "__main__":
+    s,t=map(str,input().split())
+    print(check_subsequence(s, t))
