e52991ebb33f0db6d713cee030c7d8e3722b4741
k395599 <keigo1003560@gmail.com>
2025-02-06 10:58:39 +0000

    Retry

--- ABC/300~399/360_B.py
+++ ABC/300~399/360_B.py
@@ -12,3 +12,19 @@ def check_subsequence(s,t):
 if __name__ == "__main__":
     s,t=map(str,input().split())
     print(check_subsequence(s, t))
+
+ # %%
+
+def check_vertical_reading (s,t):
+    for w in range(1,len(s)):
+        for c in range(w):
+            now=[]
+            for i in range(c,len(s),w):
+                now.append(s[i])
+            if "".join(now)==t:
+                return "Yes"
+    return "No"
+
+if __name__ =="__main__":
+    s,t = map(str,input().split())
+    print(check_vertical_reading(s,t))
