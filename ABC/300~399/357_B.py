80ce88896b245614f31d84e7d42f359b52487d85
Kubota-Kei5 <91825532+Kubota-Kei5@users.noreply.github.com>
2025-02-08 04:49:32 +0000

    complete

--- ABC/300~399/357_B.py
+++ ABC/300~399/357_B.py
@@ -1,2 +1,13 @@
 # %%
-N=input()
+S=str(input())
+L=len(S)
+upper_cnt=0
+for i in range(L):
+    if S[i].isupper():
+        upper_cnt+=1
+
+if L-upper_cnt<upper_cnt:
+    print(S.upper())
+else:
+    print(S.lower())            
+# %%
