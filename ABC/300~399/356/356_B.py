a26e83504e3f12eb30e1c3bb8b430dd0ae0fea93
k395599 <keigo1003560@gmail.com>
2025-02-15 10:05:06 +0000

    complete

--- ABC/300~399/356/356_B.py
+++ ABC/300~399/356/356_B.py
@@ -1,2 +1,16 @@
 # %%
-N=input()
+import numpy as np
+N,M=map(int,input().split())
+A=list(map(int,input().split()))
+
+for i in range(N):
+    X=list(map(int,input().split()))
+    nX=np.array(X)
+    A=A-nX
+
+for i in range(M):
+    if A[i]>0:
+        print('No')
+        exit()
+    
+print('Yes')
