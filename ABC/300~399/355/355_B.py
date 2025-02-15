d51d812cb1bc5780068b887bdf5551d46a0319ab
k395599 <keigo1003560@gmail.com>
2025-02-15 10:38:33 +0000

    complete

--- ABC/300~399/355/355_B.py
+++ ABC/300~399/355/355_B.py
@@ -1,2 +1,20 @@
-# %%
-N=input()
+N,M=map(int,input().split())
+A=list(map(int,input().split()))
+B=list(map(int,input().split()))
+
+Ad={}
+Bd={}
+for i in range(N):
+    Ad[A[i]]=1
+for i in range(M):
+    Bd[B[i]]=2
+
+Ad.update(Bd)
+l=sorted(Ad.items())
+
+for i in range(N+M-1):
+    if l[i][1]==l[i+1][1]==1:
+        print('Yes')
+        exit()
+
+print('No')
