401a482035c1c4181606807b635f888161b933f8
k395599 <keigo1003560@gmail.com>
2025-02-16 01:34:45 +0000

    complete

--- ABC/300~399/343/343_A.py
+++ ABC/300~399/343/343_A.py
@@ -1,2 +1,7 @@
 # %%
-N=input()
+A,B=map(int,input().split())
+C=A+B
+for i in range(10):
+    if i!=C:
+        print(i)
+        exit()
--- ABC/300~399/343/343_B.py
+++ ABC/300~399/343/343_B.py
@@ -1,2 +1,9 @@
 # %%
-N=input()
+N=int(input())
+for i in range(N):
+    ans=[]
+    A=list(map(int,input().split()))
+    for j in range(N):
+        if A[j]==1:
+            ans.append(j+1)
+    print(*ans)
