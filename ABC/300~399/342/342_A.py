8ee1b9831b69ee1358e0b1a4fdaf2a1d9e01214d
k395599 <keigo1003560@gmail.com>
2025-02-17 00:01:50 +0000

    complete

--- ABC/300~399/342/342_A.py
+++ ABC/300~399/342/342_A.py
@@ -1,2 +1,18 @@
 # %%
-N=input()
+S=str(input())
+major='0'
+for i in range(3):
+    if S[0]==S[1]==S[2]:
+        major=S[0]
+    elif S[0]==S[1] and S[1]!=S[2]:
+        major=S[0]
+    elif S[0]!=S[1] and S[1]==S[2]:
+        major=S[1]
+    elif S[0]==S[2] and S[2]!=S[1]:
+        major=S[0]
+
+for i in range(len(S)):
+    if S[i]!=major:
+        print(i+1)
+        exit()
+# %%
