f2dc7a81428e56343ba434378bb8936318a56a2c
k395599 <keigo1003560@gmail.com>
2025-02-17 00:02:03 +0000

    回答中

--- ABC/300~399/342/342_B.py
+++ ABC/300~399/342/342_B.py
@@ -1,2 +1,19 @@
 # %%
-N=input()
+N=int(input())
+
+P=list(map(int,input().split()))
+l=[]
+for i in range(N):
+    l.append({P[i]:i+1})
+
+print(l)
+
+Q=int(input())
+
+for i in range(Q):
+    query=list(map(int,input().split()))
+    comp=101
+    for j in range(N):
+        if l[j][0]==query[0] or l[j][0]==query[0]:
+           comp=min(comp,l[j][1])
+    print(comp) 
