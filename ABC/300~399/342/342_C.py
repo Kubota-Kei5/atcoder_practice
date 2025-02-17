3ca10ad55ea66663530320ff0f1451c18ae0eddc
Kubota-Kei5 <91825532+Kubota-Kei5@users.noreply.github.com>
2025-02-17 20:21:56 +0900

    Update 342_C.py
    retry

--- ABC/300~399/342/342_C.py
+++ ABC/300~399/342/342_C.py
@@ -1,2 +1,13 @@
 # %%
-N=input()
+N=int(input())
+S=str(input())
+Q=int(input())
+
+# ヒント：for文でtranslateをQ回処理するのが大変
+# 変換マップを作って1回のmaketransで済むようにする
+
+for _ in range(Q):
+    c,d=map(str,input().split())
+    S=S.translate(str.maketrans({c:d}))
+
+print(S)
