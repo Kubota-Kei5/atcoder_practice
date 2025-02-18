3bc4cefd413bc52b92512f602d2c324e5a8e2105
kubotake_notePC <keigo1003560@gmail.com>
2025-02-18 23:57:35 +0000

    回答中

--- ABC/300~399/340/340_C.py
+++ ABC/300~399/340/340_C.py
@@ -1,2 +1,39 @@
 # %%
-N=input()
+import math
+from collections import deque
+
+a=[]
+a.append(int(input()))
+d=deque(a)
+spend=0
+
+while d:
+    x=d.popleft()
+    if x>=2:
+        spend+=x
+        floor_x=math.floor(x/2)
+        ceil_x=math.ceil(x/2)
+
+        if floor_x != x:
+            d.append(floor_x)
+        if ceil_x != x:
+            d.append(ceil_x)
+
+print(spend)
+# %%
+import math
+from collections import deque
+
+a=[]
+a.append(int(input()))
+d=deque(a)
+spend=0
+
+while d:
+    x=d.popleft()
+    if x>=2:
+        spend+=x
+        d.append(math.floor(x/2))
+        d.append(math.ceil(x/2))
+
+print(spend)
