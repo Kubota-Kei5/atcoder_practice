0bc7cfc265253a0a02bb07eddc15531e706e7c71
kubotake_notePC <keigo1003560@gmail.com>
2025-02-18 23:28:39 +0000

    complete

--- ABC/300~399/340/340_A.py
+++ ABC/300~399/340/340_A.py
@@ -1,2 +1,9 @@
 # %%
-N=input()
+A, B, D =map(int,input().split())
+
+ans=[]
+
+for i in range(A,B+1,D):
+    ans.append(i)
+
+print(*ans)
--- ABC/300~399/340/340_B.py
+++ ABC/300~399/340/340_B.py
@@ -1,2 +1,10 @@
 # %%
-N=input()
+Q=int(input())
+A=[]
+for i in range(Q):
+    query=list(map(int,input().split()))
+
+    if query[0]==1:
+        A.append(query[1])
+    else:
+        print(A[-query[1]])
