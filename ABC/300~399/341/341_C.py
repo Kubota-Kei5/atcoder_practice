33d6b848a7b6b672c153e5e71f4f2b3cc2fc6b32
kubotake_notePC <keigo1003560@gmail.com>
2025-02-18 12:18:57 +0000

    complete

--- ABC/300~399/341/341_C.py
+++ ABC/300~399/341/341_C.py
@@ -2,39 +2,38 @@
 H,W,N=map(int,input().split())
 T=str(input())
 
-grid=[list(map(str,input().split()))]
+grid = [list(input().strip()) for _ in range(H)]
 
-def judge_move(pos_i,pos_j,move,grid):
-    if move=='L' and pos_j!=0:
-        if grid[pos_i][pos_j-1]=='.':
-            return pos_i,pos_j-1
-        else:
-            return False
-    elif move=='R' and pos_j!=len(grid[0]):
-        if grid[pos_i][pos_j+1]=='.':
-            return pos_i,pos_j+1
-        else:
-            return False
-    elif move=='U' and pos_i!=0:
-        if grid[pos_i+1][pos_j]=='.':
-            return pos_i+1,pos_j
-        else:
-            return False
-    elif move=='D' and pos_i!=len(grid):
-        if grid[pos_i-1][pos_j]=='.':
-            return pos_i-1,pos_j
-        else:
-            return False
-    else:
-        return False
+
+def judge_move(pos_i, pos_j, move, grid):
+    if move == 'L' and pos_j != 0:
+        if grid[pos_i][pos_j - 1] == '.':
+            return pos_i, pos_j - 1
+    elif move == 'R' and pos_j != W - 1:
+        if grid[pos_i][pos_j + 1] == '.':
+            return pos_i, pos_j + 1
+    elif move == 'U' and pos_i != 0:
+        if grid[pos_i - 1][pos_j] == '.':
+            return pos_i - 1, pos_j
+    elif move == 'D' and pos_i != H - 1:
+        if grid[pos_i + 1][pos_j] == '.':
+            return pos_i + 1, pos_j
+    return None
+
 cnt=0
-for h in range(H):
-    for w in range(W):
+for i in range(H):
+    for j in range(W):
+        if grid[i][j] != '.':
+            continue
+        h,w=i,j
+        valid=True
         for t in T:
-            if judge_move(h,w,t,grid)==False:
+            new_pos = judge_move(h, w, t, grid)
+            if new_pos is None:
+                valid=False
                 break
-            else:
-                h,w=judge_move(h,w,t,grid)
-        cnt+=1
-            
+            h, w = new_pos
+        if valid:
+            cnt+=1
+
 print(cnt)
