7a4b105f1740bf0218ec24185916152b71592cce
kubotake_notePC <keigo1003560@gmail.com>
2025-02-19 23:55:32 +0000

    complete

--- ABC/300~399/339/339_A.py
+++ ABC/300~399/339/339_A.py
@@ -1,2 +1,12 @@
 # %%
-N=input()
+from collections import deque
+S=str(input())
+
+ans=deque([])
+for s in reversed(S):
+    if s!='.':
+        ans.insert(0,s)
+    else:
+        print(''.join(ans))
+        exit()
+# %%
