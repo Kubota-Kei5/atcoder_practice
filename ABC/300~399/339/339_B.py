6a9035bc408c88d7a3921dabe976cf2484cfdef4
kubotake_notePC <keigo1003560@gmail.com>
2025-02-20 12:25:17 +0000

    回答中

--- ABC/300~399/339/339_B.py
+++ ABC/300~399/339/339_B.py
@@ -1,5 +1,15 @@
 # %%
+import math
 H,W,N=map(int,input().split())
 
 grid=[['.']*W]*H 
 
+cnt=0
+
+for i in range(math.ceil(N/4)):
+    
+    if cnt!=N:
+        pass
+        cnt+=1
+    else:
+        print(grid)
--- ABC/300~399/339/339_C.py
+++ ABC/300~399/339/339_C.py
@@ -1,2 +1,18 @@
 # %%
-N=input()
+N=int(input())
+A=list(map(int,input().split()))
+
+stop_index=0
+stop_value=0
+for i in range(len(A)):
+    if A[i]<0:
+        stop_index=i
+        stop_value=A[i]
+        break
+
+if stop_index==0:
+    print(abs(stop_value))
+elif sum(A[0:stop_index+1])>=0:
+    print(0)
+else:
+    print(abs(sum(A[0:stop_index+1])))
