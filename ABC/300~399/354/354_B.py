39ad7867190762bb80c88383f4a457d26ad606eb
k5 <keigo1003560@gmail.com>
2025-03-11 12:11:55 +0000

    complete

--- ABC/300~399/354/354_B.py
+++ ABC/300~399/354/354_B.py
@@ -1,2 +1,17 @@
 # %%
-N=input()
+N=int(input())
+
+list=[]
+sumRate=0
+for i in range(N):
+    userName,rate=input().split()
+    sumRate+=int(rate)
+    list.append(userName)
+
+
+# 合計値をNで割った余りを求める
+mod=sumRate%N
+
+sorted_list=sorted(list)
+
+print(sorted_list[mod])
