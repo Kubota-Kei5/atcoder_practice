b6117e9e3db0af51d95338298bd59994ae44dc4d
kubotake-desktop <keigo1003560@gmail.com>
2025-03-18 11:52:34 +0000

    Retry

--- ABC/300~399/345/345_B.py
+++ ABC/300~399/345/345_B.py
@@ -1,2 +1,12 @@
 # %%
-N=input()
+# X/10の計算でfloatになった場合、16桁の程度の精度しか持たない。
+# そのため、このコードでは10の18乗のような桁数の入力があった場合に誤差が発生する。
+import math
+X=int(input())
+
+print(math.ceil(X/10))
+
+# %%
+# このコードでAC
+X = int(input())
+print((X + 9) // 10)
