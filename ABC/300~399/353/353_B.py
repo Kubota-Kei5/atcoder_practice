2a373e081240808c5ca72e5bd60530049b9fea13
kubotake_notePC <keigo1003560@gmail.com>
2025-04-01 14:57:51 +0000

    complete

--- ABC/300~399/353/353_B.py
+++ ABC/300~399/353/353_B.py
@@ -1,2 +1,15 @@
 # %%
-N=input()
+N, K=map(int,input().split())
+A=list(map(int, input().split()))
+
+cnt=0
+seats=K
+
+for i in range(N):
+    if seats<A[i]:
+        cnt+=1
+        seats=K-A[i]
+    else:
+        seats-=A[i]
+
+print(cnt+1)
