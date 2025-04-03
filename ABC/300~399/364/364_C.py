c7938f346acb9113df9759202b5fbbbdd1ed1fbd
kubotake-desktop <keigo1003560@gmail.com>
2025-04-03 04:11:20 +0000

    実行時間は短縮だが実行時エラー検出

--- ABC/300~399/364/364_C.py
+++ ABC/300~399/364/364_C.py
@@ -30,3 +30,44 @@ if min_sweet is None and min_salty is None:
 else:
     min_cnt = min(min_sweet, min_salty)
     print(min_cnt)
+
+# %%
+# 累積和と二分探索で探索回数を短縮
+
+from itertools import accumulate
+from bisect import bisect_left
+
+N,X,Y=map(int,input().split())
+A=list(map(int, input().split()))
+B=list(map(int, input().split()))
+
+sorted_A=sorted(A,reverse=True)
+sorted_B=sorted(B,reverse=True)
+
+def sweet_strategy(X,sorted_A):
+    prefix_A = list(accumulate(sorted_A))
+    if prefix_A[-1] > X:
+        index_sweet = bisect_left(prefix_A, X)
+        sweet_cnt= index_sweet + 1
+        return sweet_cnt
+    else:
+        sweet_cnt = None
+        return sweet_cnt
+
+def salty_strategy(Y,sorted_B):
+    prefix_B= list(accumulate(sorted_B))
+    if prefix_B[-1] > Y:
+        index_salty = bisect_left(prefix_B, Y)
+        salty_cnt = index_salty + 1
+        return salty_cnt
+    else:
+        salty_cnt = None
+        return salty_cnt
+    
+min_sweet = sweet_strategy(X,sorted_A)
+min_salty = salty_strategy(Y,sorted_B)
+if min_sweet is None and min_salty is None:
+    print(N)
+else:
+    min_cnt = min(min_sweet, min_salty)
+    print(min_cnt)
