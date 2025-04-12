7d5ead8e49bf31f74288a65c1e0c06637d14304b
kubotake-desktop <keigo1003560@gmail.com>
2025-04-12 23:59:49 +0000

    complete

--- ABC/300~399/350/350_B.py
+++ ABC/300~399/350/350_B.py
@@ -1,2 +1,15 @@
 # %%
-N=input()
+N,Q=map(int,input().split())
+
+T=list(map(int,input().split()))
+
+teeth=[1]*(N+1)
+
+for i in range(Q):
+    if teeth[T[i]] == 1:
+        teeth[T[i]]=0
+    else:
+        teeth[T[i]]=1
+
+ans=sum(teeth)-1 #0_indexedにしているためindex=0の分を引く
+print(ans)
