12e98f83ce6e54fea87541844d3a4f03b9c1b5ef
kubotake-desktop <keigo1003560@gmail.com>
2025-04-23 11:19:08 +0000

    complete

--- ABC/300~399/341/341_B.py
+++ ABC/300~399/341/341_B.py
@@ -3,5 +3,15 @@ N=int(input())
 
 A=list(map(int, input().split()))
 
-for i in range(N-1):
+change_rate= []
+for _ in range(1,N):
     s,t=map(int,input().split())
+    change_rate.append((s,t))
+
+get_money=0
+
+for i in range(0,N-1):
+    get_money=(A[i]//change_rate[i][0])*change_rate[i][1]
+    A[i+1]+= get_money
+
+print(A[-1])
