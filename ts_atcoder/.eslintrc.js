416f876a94a0d6f0865cb7f6631082ce162b9d03
kubotake-desktop <keigo1003560@gmail.com>
2025-05-11 23:21:11 +0900

    typescriptの環境構築

---
+++ ts_atcoder/.eslintrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+  parser: "@typescript-eslint/parser",
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "prettier",
+  ],
+  env: {
+    node: true,
+    es2020: true,
+  },
+};
