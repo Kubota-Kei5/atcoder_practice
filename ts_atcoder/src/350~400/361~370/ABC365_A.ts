2c8759980ed3a90c9cbc6fd1e7c8925e4bc1fbf7
kubotake-desktop <keigo1003560@gmail.com>
2025-05-17 10:58:22 +0900

    prepare

--- ts_atcoder/src/350~400/361~370/ABC365_A.ts
+++ ts_atcoder/src/350~400/361~370/ABC365_A.ts
@@ -1,7 +1,7 @@
 import { readFileSync } from "fs";
 
-const lines = readFileSync(0, "utf-8").trim().split(/\s+/);
+const lines = readFileSync(0, "utf-8").trim().split(/\s+/).map(Number);
 let ptr = 0;
 
-const N = Number(lines[ptr++]);
-const X = Number(lines[ptr++]);
+const N: number = lines[ptr++];
+const T: number = lines[ptr++];
