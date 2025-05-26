c6b4be75603260ca2f233fbd624e2e271ae65e01
kubotake-desktop <keigo1003560@gmail.com>
2025-05-26 22:18:30 +0900

    complete

--- python_atcoder/ABC/300~399/391/391_C.py
+++ python_atcoder/ABC/300~399/391/391_C.py
@@ -1,7 +1,36 @@
 # %%
 # Retry
 
+N,Q = map(int, input().split())
 
+# 巣iにいる鳩の数
+n_pigeons_at = [1] * (N + 1)
+
+# 鳩iがいる巣箱
+pos = [i for i in range(N + 1)]
+
+# 複数のハトがいる巣の個数
+n_mul = 0
+
+for _ in range(Q):
+    query = input().split()
+
+    if query[0] == "1":
+        P= int(query[1])
+        new_H = int(query[2])
+        old_H = pos[P]
+
+        if n_pigeons_at[old_H] >= 2 and n_pigeons_at[old_H] -1 ==1:
+            n_mul -= 1
+        n_pigeons_at[old_H] -= 1
+
+        if n_pigeons_at[new_H] == 1 and n_pigeons_at[new_H] +1 ==2:
+            n_mul += 1
+        n_pigeons_at[new_H]+=1
+
+        pos[P] = new_H
+    else:
+        print(n_mul)
 
 
 # %%
@@ -13,7 +42,7 @@ n_mul = 0
 n_pigeons_at = [1] * (N + 1)
 # 鳩iがいる巣箱pos[i]
 pos = [i for i in range(N + 1)]
-
+    
 for _ in range(Q):
     s = input()
     if s[0] == "1":
